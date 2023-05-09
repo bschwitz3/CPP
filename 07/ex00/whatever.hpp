b66fde7bfcab9e387992463f5ea8d8eb4166ea9b
bschwitz <bschwitz@student.42lausanne.ch>
2023-05-09 18:01:24 +0200

    change 07 00

--- 07/ex00/whatever.hpp
+++ 07/ex00/whatever.hpp
@@ -6,7 +6,7 @@
 /*   By: bschwitz <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 17:24:58 by bschwitz          #+#    #+#             */
-/*   Updated: 2023/05/09 17:37:39 by bschwitz         ###   ########.fr       */
+/*   Updated: 2023/05/09 18:00:28 by bschwitz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,17 +28,13 @@ void	swap(T& v1, T& v2)
 template<typename T>
 T	min(const T& v1, const T& v2)
 {
-	if (v1 < v2)
-		return v1;
-	return v2;
+	return(v1 < v2 ? v1 : v2);
 }
 
 template<typename T>
 T	max(const T& v1, const T& v2)
 {
-	if (v1 > v2)
-		return v1;
-	return v2;
+	return (v1 > v2 ? v1 : v2);
 }
 
 #endif
